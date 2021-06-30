88ad92e3b06a995bdb0c8e80ba84899c6e7fa8c3
dahnny <danielogbuti@gmail.com>
2021-06-30 15:20:23 +0100

    minor changes

--- controllers/previewController.js
+++ controllers/previewController.js
@@ -6,8 +6,8 @@ const previewController = {
         const projectId = req.query.p;
         try {
             let project = await Project.findById(projectId);
-            let user = await User.findOne({ username: project.username });
-            res.render('preview', { user: user, project: project });
+            let foundUser = await User.findOne({ username: project.username });
+            res.render('preview', { foundUser: foundUser, project: project });
         } catch (error) {
             console.log(error);
             req.flash('error', error.message);
