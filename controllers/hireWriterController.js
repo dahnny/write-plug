36c21334178771c24ba96dffb816ec71827b8dd2
Daniel Ogbuti <danielogbuti@gmail.com>
2021-09-22 00:41:06 +0100

    changes

--- controllers/hireWriterController.js
+++ controllers/hireWriterController.js
@@ -5,13 +5,13 @@ const hireWriter = {
     res.render("hire", { user: req.user });
   },
   postWriter: async (req, res) => {
-    const { topic, budget } = req.body;
+    const { topic,course, budget } = req.body;
 
     let email = {
       from: req.user.emailAddress,
       to: "writeplugng@gmail.com",
       subject: "Hire a writer",
-      text: `Name: ${req.user.fullname}\nEmail: ${req.user.emailAddress}\nPhone Number: ${req.user.phone}\n I want to hire a writer for the topic ${topic} and with a budget of  ${budget} naira`,
+      text: `Name: ${req.user.fullname}\nEmail: ${req.user.emailAddress}\nPhone Number: ${req.user.phone}\n I want to hire a writer for the topic ${topic} under the course ${course} and with a budget of  ${budget} naira`,
     };
 
     try {
